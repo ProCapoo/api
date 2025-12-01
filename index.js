d4938f58c79d6ba30743d9509be2e10a1943dd58
ProCapoo <capoboo250@gmail.com>
2025-12-01 12:57:09 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,14 +1,11 @@
-// index.js
 const express = require('express');
 const app = express();
+const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-  res.json({ message: "Hello from Render！API 已經上線啦～" });
+  res.json({ message: 'Hello from Render API! 部署成功～' });
 });
 
-app.get('/users', (req, res) => {
-  res.json([{ id: 1, name: "小明" }, { id: 2, name: "小美" }]);
+app.listen(port, () => {
+  console.log(`API running on port ${port}`);
 });
-
-const port = process.env.PORT || 3000;
-app.listen(port, () => console.log(`API running on port ${port}`));
